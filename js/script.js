c33e2bd1ef966c932b34b4d34fbad6834da040ff
zhiyuan zhang <zzhiyuan.work@gmail.com>
2024-12-10 14:01:48 -0500

    add profile

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Example: Display current year in the footer
+const year = new Date().getFullYear();
+document.querySelector("footer p").textContent = `© ${year} Zhiyuan Zhang . All Rights Reserved.`;
